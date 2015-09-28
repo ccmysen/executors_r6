5fc6b10d595e3d5683540d7b091133e0d154d76d
ccmysen <ccmysen@users.noreply.github.com>
2015-09-27 21:03:22 -0700

    Adding some comments to system_executor which is not currently conforming to the proposal.

--- include/system_executor.h
+++ include/system_executor.h
@@ -15,12 +15,20 @@ class system_executor {
 
  public:
   static system_executor& get_executor() {
+    // Destroys the system executor during static destruction, though may need
+    // some special logic to handle doing this before statics (probably need
+    // something built in).
+    //
+    // TODO: consider making an executor registry which is just a static
+    // registry of executors which need to be deleted during static
+    // destruction. This would allow non-singleton executors to delete during
+    // this same phase.
     static system_executor instance(
         THREAD_RATIO * thread::hardware_concurrency());
     return instance;
   }
 
-  // Destructor of the system executor should probably kill stuff...
+  // NOTE: by default the internal thread pool will
   virtual ~system_executor() {}
 
  public:
@@ -36,6 +44,9 @@ class system_executor {
 
   system_executor(size_t N) : pool_(N) {}
 
+  // TODO: make the executor actually grow depending on forward progress of the
+  // threads (need to figure out a reasonable mechanism for guaranteeing
+  // forward progress.
   thread_pool_executor pool_;
 };
 
